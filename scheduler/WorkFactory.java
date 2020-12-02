d5fd9f23116ecea763b8b4e6243607ac9fb8d142
Nathan Fishel <50148601+nfishel48@users.noreply.github.com>
2020-12-01 21:40:46 -0500

    Update WorkFactory.java

--- scheduler/WorkFactory.java
+++ scheduler/WorkFactory.java
@@ -1,15 +1,6 @@
 package scheduler;
 
-/**
- * A factory class for generating JobWorkables to be placed in Jobs.
- * When I test your program I will replace this class with my own.
- * The only variability will be in createWork(), which will return
- * an instance of a class that I have created to implement JobWorkable.
- * @author matt
- * @version 1.1
- * <p>Copyright: Copyright (c) 2016, 2015, 2004 by Matt Evett</p>
- *
- */
+
 public class WorkFactory {
 	private int jobCount = 0;
 	public JobWorkable createWork() {
